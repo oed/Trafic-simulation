c4e6e5ff96455e27ac926817fc2a900925e893e0
Weery <vicnilss@student.chalmers.se>
2015-11-23 16:06:48 +0100

    Basic road

--- main.py
+++ main.py
@@ -1,18 +1,35 @@
 #!/usr/bin/env python2
+import sys, pygame
+from road import Road
 
+pygame.init()
+size = width, height = 620, 440
+
+screen = pygame.display.set_mode(size)
 time_interval = 0.5
 
 car_list = []
+car_Road = Road("something")
 
-def init_cars():
-    # TODO - add some cars to the car list
+#def init_cars():
 
+	# TODO - add some cars to the car list
+
+#def init_road():
+	#car_Road = Road()
 
 def mainloop():
-    for car in car_list:
-        car.update(time_interval)
-
+	while 1:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT: sys.exit()
+		screen.fill((0,0,0))
+		car_Road.Draw(screen,pygame)
+		pygame.display.flip()
+		for car in car_list:
+			car.update(time_interval)
+			
 
 if __name__ == '__main__':
-    init_cars()
-    mainloop()
+	#init_cars()
+	#init_road()
+	mainloop()
--- road.py
+++ road.py
@@ -1,9 +1,30 @@
-class Road:
 import pygame
-nodes:
-    def __init__(self):
-	    self.nodes=[]
+class Road(object):
+
+	nNodes=0
+
+	def __init__(self, text_Name):
+		self.nodes=[]
+		#TEMP
 		self.nodes.append((1,1))
+		self.nodes.append((100,100))
+		self.nodes.append((120,50))
+		self.nodes.append((250,300))
+		self.nodes.append((10,220))
+		#TEMP
+		self.LoadNodesFromFile(text_Name)
+		self.nNodes=len(self.nodes)
+		print self.nNodes
+
+	def Draw(self, screen, pygame):
+		pygame.draw.lines(screen,(125,125,125),True,self.nodes,10)
+	
+	
+	def LoadNodesFromFile(self, text_Name):
+		print self.nodes
+
+	def GetNextNode(self, current_Node)
+		return (current_Node+1)%self.nNodes
 		
-    def draw(screen,pygame):
-	    pygame.draw.lines(screen,(120,120,120),self.nodes)
+	def GetNodePosition(self, nNode)
+		return self.nodes[nNode]
