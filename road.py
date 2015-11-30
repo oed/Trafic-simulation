2674831c2d518ab8628a83fedf45e0fee84ce424
Weery <vicnilss@student.chalmers.se>
2015-11-30 15:20:49 +0100

    Draw roads

--- road.py
+++ road.py
@@ -4,33 +4,37 @@ import pickle
 
 class Road(object):
 
-
-    def __init__(self, text_Name):
-        self.LoadNodesFromFile(text_Name)
+	def __init__(self, text_Name):
+		self.LoadNodesFromFile(text_Name)
 
 	def Draw(self, screen, pygame):
-		self.DrawList(screen,pygame,roads['Main'])
-		self.DrawList(screen,pygame,[x[0] for x in roads['Start']])
-		self.DrawList(screen,pygame,[x[0] for x in roads['End']])
+		self.DrawList(screen,pygame,self.roads['Main'])
+		self.DrawNodeLines(screen,pygame,self.roads['Start'],self.roads['Main'],(0,255,0))
+		self.DrawNodeLines(screen,pygame,self.roads['End'],self.roads['Main'],(255,0,0))
 
 	def DrawList(self,screen,pygame,node_List):
 		pygame.draw.lines(screen,(125,125,125),True,node_List,10)
 
-    def LoadNodesFromFile(self, text_Name):
+	def DrawNodeLines(self,screen,pygame,node_List_With_Index,node_List,color):
+		for iNode in range(1,len(node_List_With_Index)):
+			pygame.draw.line(screen,color,node_List_With_Index[iNode][0],node_List[node_List_With_Index[iNode][1]],5)
+		
+		
+	def LoadNodesFromFile(self, text_Name):
 		f = open(text_Name)
 		self.roads = pickle.load(f)
 		self.nNodes=len(self.roads['Main'])
 		f.close()
 
-    def GetNextNode(self, current_Node,exit_Probability):
+	def GetNextNode(self, current_Node,exit_Probability):
         # TODO - implement logic for exits and starts
         #if (random.random()< exit_Probability):
             #exitNode = self.FindConnectedExit(current_Node)
             #if exitNode[0]!=-1:
                 #return exitNode
-        return (current_Node+1)%self.nNodes
+		return (current_Node+1)%self.nNodes
 
-    def GetNodePosition(self, nNode):
+	def GetNodePosition(self, nNode):
         # TODO - make it work with exits and starts
         #if nNode[1]==0:
             #return self.nodes[nNode]
@@ -38,10 +42,10 @@ class Road(object):
             #return self.ExitNodes[nNode][0]
         #elif nNodes[1]==2:
             #return self.EntranceNodes[nNodes][0]
-        return self.roads['Main'][nNode]
+		return self.roads['Main'][nNode]
 
-    def FindConnectedExit(self, nNode):
-        for x in range(0,len(self.ExitNodes)):
-            if self.ExitNodes[x,1]==nNode:
-                return (x,1)
-        return (-1,-1)
+	def FindConnectedExit(self, nNode):
+		for x in range(0,len(self.ExitNodes)):
+			if self.ExitNodes[x,1]==nNode:
+				return (x,1)
+		return (-1,-1)
