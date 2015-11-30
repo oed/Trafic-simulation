85fbb3d35bc8b6553e3aa586f9937ea5ab99cc05
Joel Torstensson <oed3933@gmail.com>
2015-11-30 14:22:23 +0100

    Broke out helpfull functions from Car.

--- car.py
+++ car.py
@@ -1,13 +1,16 @@
 import random
 import math
+import utils
 
 max_velocity = 5 #Class variable shared by all instances
 max_acceleration = 2
-range_of_sight = 3
+range_of_sight = 20
 exit_probability = 0
 
 
 class Car:
+
+    car_number = 0;
 
     def __init__(self, startNode,road):
         self.startNode = startNode    # instance variable unique to each instance
@@ -18,8 +21,9 @@ class Car:
         self.velocity = max_velocity*random.random()
         self.acceleration = max_acceleration*random.random()
         self.nextNode = self.road.GetNextNode(startNode, exit_probability)
-        #self.direction = get_direction(visitedNodes[-1])
         self.direction = self.get_direction()
+        self.car_number = Car.car_number
+        Car.car_number += 1
 
     def update(self, cars, delta_t):
 
@@ -30,17 +34,18 @@ class Car:
 
         for car in cars: #Loop over all cars, but should exclude the self
 
-            distance = getDistance(self.position,car.position)
+            distance = utils.calc_distance(self.position, car.position)
             if distance == 0:
                 continue
             elif distance <= range_of_sight:
                 print "I can see another car"
                 acceleration = -self.acceleration
 
+        print self.car_number
         self.velocity = self.velocity + acceleration*delta_t
 
         next_pos = self.road.GetNodePosition(self.nextNode)
-        if getDistance(self.position, next_pos) < self.velocity: #We arrive at the next node
+        if utils.calc_distance(self.position, next_pos) < self.velocity: #We arrive at the next node
             self.currentNode = self.nextNode
             self.visitedNodes.append(self.currentNode)
             self.nextNode = self.road.GetNextNode(self.currentNode, exit_probability)
@@ -60,17 +65,9 @@ class Car:
 
     def get_direction(self):
         next_pos = self.road.GetNodePosition(self.nextNode)
-        opposite = self.position[1] - next_pos[1]
-        adjacent = self.position[0] - next_pos[0]
-        angle = math.atan(opposite / adjacent)
-        if self.position[0] > next_pos[0]:
-            angle += math.pi
-        return angle
+        return utils.calc_angle(self.position, next_pos)
 
     def draw(self, screen, pygame):
         position = (int(self.position[0]), int(self.position[1]))
         pygame.draw.circle(screen, (255, 255, 0), position, 5)
-
-def getDistance(pos1,pos2):
-    return math.sqrt(pow((pos1[0]-pos2[0]),2)+pow((pos1[1]-pos2[1]),2))
 
---
+++ utils.py
@@ -0,0 +1,15 @@
+import math
+
+
+def calc_distance(pos1, pos2):
+    """Calculate the distance between two positions."""
+    return math.sqrt(pow((pos1[0]-pos2[0]), 2)+pow((pos1[1]-pos2[1]), 2))
+
+def calc_angle(pos1, pos2):
+    """Calculate the angle between two positions."""
+    opposite = pos1[1] - pos2[1]
+    adjacent = pos1[0] - pos2[0]
+    angle = math.atan(opposite / adjacent)
+    if pos1[0] > pos2[0]:
+        angle += math.pi
+    return angle
